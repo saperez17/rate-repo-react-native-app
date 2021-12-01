a8418a0b7a4545f5c801ee3abd126e3dc8c4c0d6
saperez17 <saperez@unicauca.edu.co>
2021-12-01 01:59:55 -0500

    added lint

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  console.log('hello world!')
+  console.log("hello world!");
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -15,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
